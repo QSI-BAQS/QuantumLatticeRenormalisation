dad57ac65fca3c072b36850cd3087f6bb2797afc
Ivelieu <dna524.hive@gmail.com>
2020-07-31 09:57:42 +1000

    clean up VF2Wrapper.cpp remove erroneous CMakeLists.txt entry

--- basic_includes.hpp
+++ basic_includes.hpp
@@ -24,13 +24,12 @@ typedef std::list<CartesianGraph> graph_list;
 // direction for cartesian logic
 enum direction { right = 0 , up = 1, back = 2, left = 3 , down = 4 , front = 5 };
 
-// vectors (follow inheritance so we can easily change 2d to 3d)
-struct vec {};
-struct vec2d : vec {
+// vectors
+struct vec2d {
     int x;
     int y;
 };
-struct vec3d : vec {
+struct vec3d {
     int x;
     int y;
     int z;
