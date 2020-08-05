50ac000412df7d05fd3970112230388be0c77fec
Ivelieu <dna524.hive@gmail.com>
2020-08-05 20:57:55 +1000

    add Y test consider LPMO vs PLMO (PLMO probably less ambiguous about how Pauli is related pp Pauli Local Measurement Operator)

--- Graph/operators.cpp
+++ Graph/operators.cpp
@@ -71,7 +71,7 @@ std::list<std::list<int>> combinations(int N, int K) {
 // for certain K related to N though, for optimisation in the measurements.
 std::list<std::list<std::list<int>>> combinations_forall_K(int N) {
     std::list<std::list<std::list<int>>> big_list;
-    for (int i = 1; i < N; i++) {
+    for (int i = 1; i <= N; i++) {
         big_list.push_back(combinations(N, i));
     }
     return big_list;
@@ -79,7 +79,7 @@ std::list<std::list<std::list<int>>> combinations_forall_K(int N) {
 
 // Inverted measurement operators
 // Non-cartesian approach
-graph_list_raw CartesianGraph::inverted_Z_measure() {
+graph_list_raw CartesianGraph::inverted_Z_measure() const {
     // temporary reference copy
     graph_t g_temp(g);
     // Output data structure
@@ -90,27 +90,95 @@ graph_list_raw CartesianGraph::inverted_Z_measure() {
     // For every permutation of order 1..|g|
     // Pretty messy loops... Will probably optimise with heuristics later
     auto combinations = combinations_forall_K(num_vertices(g));
-    for (auto combination_size_k : combinations) {
-        for (const auto &combination : combinations) {
-            for (const auto &comb : combination) {
-                // copy to g_dash
-                graph_t g_dash(g_temp);
-                for (auto ii : comb) {
-                    // add edges to the matching permutations
-                    add_edge(A, ii, g_dash);
-                }
-                // add to our global set
-                out.push_back(g_dash);
+    for (const auto &combination_size_k : combinations) {
+        for (const auto &combination : combination_size_k) {
+            // copy to g_dash
+            graph_t g_dash(g_temp);
+            for (auto ii : combination) {
+                // add edges to the matching permutations
+                add_edge(A, ii, g_dash);
             }
+            // add to our global set
+            out.push_back(g_dash);
+
         }
     }
     return out;
 }
 
-graph_list_raw CartesianGraph::inverted_Y_measure() {
+graph_list_raw CartesianGraph::inverted_Y_measure() const {
+    // Similar to Z_measure, except we perform an orbital after we add edges
+    // Would be pretty complex to reduce code duplication here... Should be fine.
 
+    // temporary reference copy
+    graph_t g_temp(g);
+    // Output data structure
+    graph_list_raw out;
+    // Add node A - won't have coordinates in this version
+    auto A = add_vertex(g_temp);
+
+    // For every permutation of order 1..|g|
+    // Pretty messy loops... Will probably optimise with heuristics later
+    auto combinations = combinations_forall_K(num_vertices(g));
+    for (const auto &combination_size_k : combinations) {
+        for (const auto &combination : combination_size_k) {
+            // copy to g_dash
+            graph_t g_dash(g_temp);
+            for (auto ii : combination) {
+                // add edges to the matching permutations
+                add_edge(A, ii, g_dash);
+            }
+            // Construct g_dash into CartesianGraph
+            CartesianGraph cg(g_dash, false);
+            // Orbital on A
+            cg.orbital(A);
+
+            // add to our global set
+            out.push_back(cg.g);
+        }
+    }
+    return out;
 }
 
-graph_list_raw CartesianGraph::inverted_X_measure() {
+graph_list_raw CartesianGraph::inverted_X_measure() const {
+    // Again, similar to Y_measure, except we get an additional factor of growth in this one.
+    // Since, for any permutation there can be one special neighbor among any of A's neighbors
+    // we get additional results, and need to have another corresponding loop.
+    // Since order matters for orbitals,
+    // we have to perform the orbital on A multiple times per inner-loop.
 
+    // temporary reference copy
+    graph_t g_temp(g);
+    // Output data structure
+    graph_list_raw out;
+    // Add node A - won't have coordinates in this version
+    auto A = add_vertex(g_temp);
+
+    // For every permutation of order 1..|g|
+    // Pretty messy loops... Will probably optimise with heuristics later
+    auto combinations = combinations_forall_K(num_vertices(g));
+    for (const auto &combination_size_k : combinations) {
+        for (const auto &combination : combination_size_k) {
+            // copy to g_dash
+            graph_t g_dash(g_temp);
+            for (auto ii : combination) {
+                // add edges to the matching permutations
+                add_edge(A, ii, g_dash);
+            }
+
+            for (auto ii : combination) {
+                // Construct g_dash into CartesianGraph (cg_dash2 because two dashes, and cartesian)
+                CartesianGraph cg_dash2(g_dash, false);
+
+                // Orbital on A
+                cg_dash2.orbital(A);
+                // Orbital on p'
+                cg_dash2.orbital(ii);
+
+                // add to our global set
+                out.push_back(cg_dash2.g);
+            }
+        }
+    }
+    return out;
 }
